bb76f673ddf05fe6a89206766a572f6d577fe2b7
BhojrajCSE21 <bhojrajnarware@gmail.com>
2024-06-29 13:13:31 +0530

    login and register

--- App.js
+++ App.js
@@ -1,68 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, Text, View, Image } from "react-native";
-import { FontAwesome } from "@expo/vector-icons";
-import Homepage from "./screens/Homepage";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigator from './navigation/AppNavigator';
 
-export default function App() {
-  const profileLogo = require("./assets/Profile.png");
+const App = () => {
   return (
-    <View style={styles.container}>
-      <SafeAreaView>
-        <StatusBar style="auto" />
-        <View style={styles.appBar}>
-          <View style={styles.leftSection}>
-            <FontAwesome name="bars" size={24} color="black" />
-          </View>
-          <View style={styles.centerSection}>
-            <Text style={styles.appBarText}>BWP</Text>
-          </View>
-          <View style={styles.rightSection}>
-            <Image source={profileLogo} style={styles.profileLogo} />
-          </View>
-        </View>
-        <Homepage />
-      </SafeAreaView>
-    </View>
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    paddingTop: 30,
-  },
-  appBar: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    paddingHorizontal: 16,
-    paddingVertical: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: "#ccc",
-    backgroundColor: "#E3EBFF",
-    marginBottom: 14,
-  },
-  leftSection: {
-    width: 50, 
-  },
-  centerSection: {
-    flex: 1,
-    alignItems: "center",
-  },
-  appBarText: {
-    fontSize: 18,
-    fontWeight: "bold",
-  },
-  rightSection: {
-    width: 50, 
-    alignItems: "flex-end",
-  },
-  profileLogo: {
-    width: 30,
-    height: 30,
-    borderRadius: 20,
-  },
-});
+export default App;
